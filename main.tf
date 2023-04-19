b6f29e52239e9ef38bb3f0055912c72a4aac768e
Rafael Soares Matos <rmatos@cpqd.com.br>
2023-04-18 21:22:13 -0300

    Adicionando ansible

--- main.tf
+++ main.tf
@@ -18,8 +18,14 @@ resource "aws_instance" "app_server" {
     ami = "ami-0aa2b7722dc1b5612"
     instance_type = "t2.micro"
     key_name = "iac-rsmatos"
+    # user_data = <<-EOF
+    #                #!/bin/bash
+    #                cd /home/ubuntu
+    #                echo "<h1>Feito pelo terraform</h1>" > index.html
+    #                nohup busybox httpd -f -p 8080 &
+    #                EOF
     tags = {
-        Name = "Primeira instancia"
+        Name = "Teste aws"
     }
 }
 
